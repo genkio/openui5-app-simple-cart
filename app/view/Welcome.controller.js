0dafe47474b94d925c8a1c7ddd329ae7eb80bb4c
j1wu <ji.wu@outlook.com>
2015-06-12 10:56:25 +0800

    added a background image to the welcome page

--- app/view/Welcome.controller.js
+++ app/view/Welcome.controller.js
@@ -3,5 +3,9 @@ sap.ui.controller("view.Welcome", {
 	    // set mock model
 	    var oModel = new sap.ui.model.json.JSONModel("model/tile.json");
 	    this.getView().setModel(oModel);
+
+	    // set background
+	    var page = this.getView().byId("welcomePage");
+	    page.addStyleClass("welcomePageBackgroundStyle");
 	}
 });
